4bd5428ed912a760cc46a1105de3d91b13612b3c
Duncan Marsh <slugbyte@slugbyte.com>
2018-12-10 19:47:18 +0800

    added readme header image

---
+++ stack/index.js
@@ -0,0 +1,26 @@
+// A JAVASCRIPT STACK IMPLAMENTATION
+
+const stackCreator = () => {
+  const state = {
+    data: {},
+    top: 0,
+  }
+
+  return {
+    push: (value) => {
+      state.data[state.top++] = value
+      return state.top
+    },
+    pop: () => {
+      if(state.top < 1) return null
+      return state.data[--state.top]
+    },
+    peek: () => {
+      if(state.top < 1) return null
+      return state.data[state.top - 1]
+    },
+    isEmpty: () => state.top == 0,
+  }
+}
+
+module.exports = createStack 
